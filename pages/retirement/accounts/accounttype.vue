b21e150c2c2b594a1033e2774dc6443b048b7241
mariomarine <rwbleo@gmail.com>
2019-01-30 12:35:37 -0700

    Fill in some information for common Retirement Acc

--- pages/retirement/accounts/accounttype.vue
+++ pages/retirement/accounts/accounttype.vue
@@ -10,6 +10,21 @@
 			<p>
 				Depending on your situation, you may choose Roth or Traditional (or probably both).
 			</p>
+			<p>
+				The simple explanation is:
+				<ul>
+					<li>
+						If you expect to live on more money than you are making now, choose Roth
+					</li>
+					<li>
+						If you expect to be spending less when you retire than you are now, choose Traditional.
+					</li>
+				</ul>
+				Personally, I expect to be spending quite a bit more during retirement (think medical expenses, spoiling grandkids, traveling, etc...), so I max out my Roth Contributions first.
+			</p>
+			<p>
+				However, if you go the Roth route, be aware that you will still have tax deductions when you retire, and if you have no taxable income (aka traditional contributions), you will be giving up some free taxes. So contributing at least a little to a traditional retirement account is often more beneficial than choosing Roth only...
+			</p>
 		</div>
 	</BasePage>
 </template>
--- pages/retirement/accounts/sponsortype.vue
+++ pages/retirement/accounts/sponsortype.vue
@@ -10,6 +10,23 @@
 			<p>
 				You don't have to always "choose" between an IRA or a 401k, because you can have both. However, in general, there are reasons to contribute to one before the other.
 			</p>
+			<p>
+				My recommended order (90% of the time) is:
+				<ol>
+					<li>
+						Contribute enough to get the full employer match in 401k. (This is free money, which is usually better than quibbling about when to pay taxes)
+					</li>
+					<li>
+						Max out your Roth IRA.
+					</li>
+					<li>
+						Finish your 401k contributions.
+					</li>
+					<li>
+						Invest in some other form of TSA (Tax Sheltered Account), such as an <nuxt-link to="/retirement/accounts/hsa">HSA</nuxt-link>, Real Estate, Whole Life Insurance, etc...
+					</li>
+				</ol>
+			</p>
 		</div>
 	</BasePage>
 </template>
